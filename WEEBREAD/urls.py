3367408e2c2cf532f6fed5f15fbbfbecee64c187
Failzuma <ibnuilham720@gmail.com>
2022-10-25 18:29:47 +0700

    minor changes

--- WEEBREAD/urls.py
+++ WEEBREAD/urls.py
@@ -1,4 +1,4 @@
-"""komikato URL Configuration
+"""WEEBREAD URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
